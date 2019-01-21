77d12417cd930822a6290d87b39cb02d2b1bcfc1
Travis Thieman <travis.thieman@gmail.com>
2019-01-21 17:05:10 -0500

    Make timing more consistent

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,55 @@
 import * as vscode from 'vscode';
 
-let timer: NodeJS.Timer;
+interface LastCheck {
+	position: vscode.Position;
+	time: number;
+}
+
+let checkTimer: NodeJS.Timer;
 let subscription: vscode.Disposable;
+let lastCheck: LastCheck;
+
+function currentPosition(): vscode.Position | undefined {
+	const editor = vscode.window.activeTextEditor;
+
+	if (editor !== undefined && editor.selection.isEmpty) {
+		return editor.selection.active;
+	}
+	return undefined;
+}
+
+function maybeTriggerHover(): void {
+	const position = currentPosition();
+	if (position === undefined) { return; }
+
+	const now = Date.now();	
+
+	if (lastCheck === undefined || position !== lastCheck.position) {
+		lastCheck = {position, time: now};
+		return;
+	}
+
+	if (now > lastCheck.time + vscode.workspace.getConfiguration('hover-cursor').delay) {
+		console.log('Running hover');
+		lastCheck = {position, time: now};
+		vscode.commands.executeCommand('editor.action.showHover');		
+	}	
+}
 
 function startTimer(): void {
-	timer = setInterval(() => { vscode.commands.executeCommand('editor.action.showHover'); },
-			vscode.workspace.getConfiguration('hover-cursor').delay);
+	checkTimer = setInterval(maybeTriggerHover, 100);
 }
 
 export function activate(context: vscode.ExtensionContext) {
 	startTimer();
 
 	subscription = vscode.workspace.onDidChangeConfiguration((event) => {
-		clearInterval(timer);
+		clearInterval(checkTimer);
 		startTimer();
 	});
 }
 
 export function deactivate() {
-	clearInterval(timer);
+	clearInterval(checkTimer);
 	subscription.dispose();
 }
